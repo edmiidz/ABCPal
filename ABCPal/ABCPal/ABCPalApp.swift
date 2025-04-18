f9d3397542a07c80e69c8b468f9e7ccbace69b39
Nik Edmiidz <edmiidz@gmail.com>
2025-04-18 10:54:49 -0700

    Initial commit

---
+++ ABCPal/ABCPal/ABCPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABCPalApp.swift
+//  ABCPal
+//
+//  Created by Nik Edmiidz on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ABCPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
